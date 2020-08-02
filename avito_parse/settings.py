2530a49fd66d1b7953abdd6dd9760fbbcf1cc184
Sergei Kalinichenko <kalinichenko.minsk@yandex.ru>
2020-08-02 12:33:07 +0300

    lesson 04, updated

--- avito_parse/settings.py
+++ avito_parse/settings.py
@@ -22,18 +22,18 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 4
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 6
+DOWNLOAD_DELAY = 10
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 16
 CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = True
 
 # Disable Telnet Console (enabled by default)
 TELNETCONSOLE_ENABLED = False
@@ -64,22 +64,23 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'avito_parse.pipelines.AvitoParsePipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'avito_parse.pipelines.AvitoParsePipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+DOWNLOAD_DELAY = 3
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_START_DELAY = 3
 # The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
+AUTOTHROTTLE_MAX_DELAY = 15
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
@@ -88,3 +89,8 @@ DEFAULT_REQUEST_HEADERS = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# ROTATING_PROXY_LIST = [
+#     '190.152.71.30:58727',
+#     '189.127.106.16:53897'
+# ]
--- avito_parse/spiders/avito.py
+++ avito_parse/spiders/avito.py
@@ -1,33 +1,53 @@
 import scrapy
 from scrapy.http import Response
+from scrapy.loader import ItemLoader
 from pymongo import MongoClient
 
 class AvitoSpider(scrapy.Spider):
     name = 'avito'
     allowed_domains = ['www.avito.ru']
-    start_urls = ['https://www.avito.ru/kurskaya_oblast/nedvizhimost',
-                  'https://www.avito.ru/kurskaya_oblast/kvartiry/prodam-ASgBAgICAUSSA8YQ']
+    start_urls = ['https://www.avito.ru/kurskaya_oblast/kvartiry/prodam-ASgBAgICAUSSA8YQ?cd=1'
+                  ]
+
     xpath_query = {
         'menu_links': '//li[contains(@class, "rubricator-list-item-item-tP77G")]//a/@href',
-        'ads_items': '//div[contains(@class, "item__line")]//h3//a[contains(@class, "snippet-link"]',
+        'ads_items': '//div[contains(@class, "item__line")]//h3//a[contains(@class, "snippet-link")]',
+        'pagination_links': '//div[contains(@class, "pagination-pages.clearfix")]//a[contains(@class, "pagination-page")]',
+        'item_params': '//div[contains(@class, "item-params")]/ul[contains(@class, "item-params-list")]',
+        'single_param': '//div[contains(@class, "item-params")]/span[contains(@class, "item-params-label")]'
     }
 
-    data_base = MongoClient()
+    data_base_client = MongoClient()
+    avito_nedviga = []
 
     def parse(self, response: Response):
-        for link in response.xpath(self.xpath_query('menu_links')):
+        for link in response.xpath(self.xpath_query['menu_links']):
             yield response.follow(link, callback=self.ads_feed_parse)
 
     def ads_feed_parse(self, response: Response):
-        for link in response.xpath(self.xpath_query('ads_item')):
+        for link in response.xpath(self.xpath_query['ads_items']):
             yield response.follow(link, callback=self.ads_item_parse)
 
     def ads_item_parse(self, response: Response):
         title = response.xpath('//h1/span[@itemprop="name"]/text()').extract_first()
-        price = response.xpath('//span[@itemprop="rpice"]/@content').extract_first()
-        yield {'title': title, 'price': price}
+        url = response.url
+        price = response.xpath('//span[@itemprop="price"]/@content').extract_first()
+        parameters = []
+        if response.xpath(self.xpath_query['item_params']):
+            for item in response.xpath(self.xpath_query['item_params']):
+                parameters.extend(item.xpath('//li[contains(@class, "item-params-list-item"]/span[contains(@class, "item-params-label")]').extract())
+        elif response.xpath(self.xpath_query['single_param']):
+            key = response.xpath('//div[contains(@class, "item-params")]/span/span/text()[1]').extract_first()
+            value = response.xpath('//div[contains(@class, "item-params")]/span/text()[2]').extract_first()
+            parameters.append({key: value})
+            # parameters.extend(response.xpath(self.xpath_query['single_param']).extract())
+        yield self.avito_nedviga.append({'title': title, 'price': price, 'url': url, 'params': parameters})
 
-#    def save_to_mongo(self):
-#        db = self.data_base['avito_gd']
-#        collection = db['avito_nedviga']
-#        pass
+    def pagination_parse(self, response: Response):
+        for link in response.xpath(self.xpath_query['pagination_links']):
+            yield response.follow(link, callback=self.ads_feed_parse)
+
+    def save_to_mongo(self):
+        db = self.data_base_client['avito_db']
+        collection = db['avito_nedviga']
+        collection.insert_many(self.avito_nedviga)
